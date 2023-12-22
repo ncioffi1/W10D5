d3f59d54252597e3c27385d9247f5e9659994288
Peter Nolan <peter.l.nolan@gmail.com>
2023-12-22 15:23:46 -0500

    finish porject

--- aa14-react-hooks-useEffect-turkey/src/App.jsx
+++ aa14-react-hooks-useEffect-turkey/src/App.jsx
@@ -1,17 +1,52 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Message from './components/Message';
 import PictureDisplay from './components/PictureDisplay';
 
 function App() {
-  const [size, setSize] = useState('s');
+  const [size, setSize] = useState('m');
+  const [sizeClass, setSizeClass] = useState('');
   const [featherCount, setFeatherCount] = useState(0);
   const [featherColors, setFeatherColors] = useState([]);
   const [isRed, setIsRed] = useState(false);
-  const [isOrange, setIsOrange] = useState(false);
+  const [isOrange, setIsOrange] = useState(false);``
   const [isBrown, setIsBrown] = useState(false);
   const [isLightBrown, setIsLightBrown] = useState(false);
   const [isYellow, setIsYellow] = useState(false);
 
+  useEffect(() => {
+    // console.log('Color Change :: red?', isRed);
+    // console.log('Color Change :: orange?', isOrange);
+    // console.log('Color Change :: brown?', isBrown);
+    // console.log('Color Change :: light brown?', isLightBrown);
+    // console.log('Color Change :: yellow?', isYellow);
+    const colors = [];
+    if (isRed) colors.push('red');
+    if (isOrange) colors.push('orange');
+    if (isBrown) colors.push('brown');
+    if (isLightBrown) colors.push('light-brown');
+    if (isYellow) colors.push('yellow');
+    setFeatherColors(colors);
+  }, [isRed, isOrange, isBrown, isLightBrown, isYellow]);
+
+  useEffect(() => {
+    let cname = '';
+    switch (size) {
+      case 'm':
+        cname = 'medium';
+        break;
+      case 'l':
+        cname = 'large';
+        break;
+      case 'xl':
+        cname = 'xlarge';
+        break;
+      default:
+        cname = 'small';
+        break;
+    }
+    setSizeClass(cname);
+  }, [size]);
+  
   return (
     <>
       <h1>Turkey Creator</h1>
@@ -20,20 +55,24 @@ function App() {
       {/* User controls */}
       <div className="button-controls">
         Size:
-        <button onClick={() => setSize('s')}>Small</button>
-        <button onClick={() => setSize('m')}>Medium</button>
-        <button onClick={() => setSize('l')}>Large</button>
-        <button onClick={() => setSize('xl')}>X-Large</button>
+        <button disabled={sizeClass === 'small'}  onClick={() => setSize('s')}>Small</button>
+        <button disabled={sizeClass === 'medium'} onClick={() => setSize('m')}>Medium</button>
+        <button disabled={sizeClass === 'large'} onClick={() => setSize('l')}>Large</button>
+        <button disabled={sizeClass === 'xlarge'} onClick={() => setSize('xl')}>X-Large</button>
       </div>
       <div className="button-controls">
         Feather Count:
-        <input
-          type="number"
-          onChange={(e) => setFeatherCount(e.currentTarget.value)}
-          defaultValue={0}
-          min={0}
-          max={10}
-        />
+        <form 
+        type="number"
+        value={featherCount > 10 ? 10 : (featherCount < 0 ? 0 : featherCount) }>
+          <input
+            type="number"
+            onChange={(e) => setFeatherCount(e.currentTarget.value)}
+            defaultValue={0}
+            min={0}
+            max={10}
+          />
+        </form>
       </div>
       <div className="button-controls">
         Feather Color(s):
@@ -62,11 +101,11 @@ function App() {
       {/* Generated display based on user selections above */}
       <h3 className="button-controls">Enjoy your turkey</h3>
       <PictureDisplay
-        size={size}
+        sizeClass={sizeClass}
         featherCount={featherCount}
         featherColors={featherColors}
       />
-      <Message size={size} />
+      <Message sizeClass={sizeClass} featherCount={featherCount}/>
     </>
   );
 }
--- aa14-react-hooks-useEffect-turkey/src/components/PictureDisplay.jsx
+++ aa14-react-hooks-useEffect-turkey/src/components/PictureDisplay.jsx
@@ -9,6 +9,7 @@ import feather7 from '../images/feather7.svg';
 import feather8 from '../images/feather8.svg';
 import feather9 from '../images/feather9.svg';
 import featherA from '../images/featherA.svg';
+import { useEffect, useState } from 'react';
 
 const feathers = [
   feather1,
@@ -23,18 +24,32 @@ const feathers = [
   featherA,
 ];
 
-function PictureDisplay ({ size, featherCount, featherColors }) {
-  console.log('PictureDisplay', size, featherCount, featherColors);
+function PictureDisplay ({ sizeClass, featherCount, featherColors }) {
+  // useEffect(() => {
+  //   console.log('PictureDisplay', size, featherCount, featherColors);
+  // }, [size, featherCount, featherColors]);
+
+  const [colors, setColors] = useState([]);
+
+  useEffect(() => {
+    console.log('PictureDisplay feather count', featherCount);
+  }, [featherCount]);
+
+  useEffect(() => {
+    console.log('PictureDisplay feather colors', featherColors);
+  }, [featherColors]);
 
   // TODO: Wrap in useEffect
-  const colors = [];
+  useEffect(() => {
+    const _colors = [];
   if (!featherColors || featherColors.length === 0) featherColors = [''];
   for (let i=0; i<featherCount; i++) {
-    colors.push(featherColors[i % featherColors.length]);
-  }
+    _colors.push(featherColors[i % featherColors.length]);
+    setColors(_colors)
+  }}, [featherColors, featherCount])
 
   return (
-    <div className={`image-area medium`}>
+    <div className={`image-area ${sizeClass}`}>
       {colors.map((c, i) =>
         <img
           key={feathers[i]}
